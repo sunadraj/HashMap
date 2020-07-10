341635a17bfea3b46b64bc486f58b1443d045913
sunad raj <40906488+sunadraj@users.noreply.github.com>
2020-07-10 20:14:34 +0530

    HashMap
    HashMap is a part of Java’s collection since Java 1.2. It provides the basic implementation of the Map interface of Java. It stores the data in (Key, Value) pairs.
    
    These are various important hashmap class methods. This post explains: put(), get(), isEmpty() and size()
    
    put(): java.util.HashMap.put() plays role in associating the specified value with the specified key in this map. If the map previously contained a mapping for the key, the old value is replaced.
    
    get(): java.util.HashMap.get()method returns the value to which the specified key is mapped, or null if this map contains no mapping for the key.
    
    isEmpty(): java.util.HashMap.isEmpty() method returns true if the map contains no key-value mappings.
    
    size(): java.util.HashMap.size() returns the number of key-value mappings in this map.

---
+++ Traversal_of_HashMap.java
@@ -0,0 +1,19 @@
+// Java program to illustrate 
+// Java.util.HashMap 
+
+import java.util.HashMap; 
+import java.util.Map; 
+
+public class GFG { 
+	public static void main(String[] args) 
+	{ 
+		HashMap<String, Integer> map = new HashMap<>(); 
+
+		map.put("vishal", 10); 
+		map.put("sachin", 30); 
+		map.put("vaibhav", 20); 
+
+		for (Map.Entry<String, Integer> e : map.entrySet()) 
+			System.out.println(e.getKey() + " " + e.getValue()); 
+	} 
+} 
